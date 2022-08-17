ce359c3af723f3744262fffec2849ec991c96884
Gustavo Adami <gstvadami@gmail.com>
2022-08-17 00:53:14 -0300

    Update ex087 - Mais sobre matriz em Python.py

--- ex087 - Mais sobre matriz em Python.py
+++ ex087 - Mais sobre matriz em Python.py
@@ -1,54 +1,23 @@
-lista = list()
-linha1 = list()
-linha2 = list()
-linha3 = list()
-pares = list()
-impares = list()
-maior = menor = 0
-
-for a in range(0,3):
-    linha1.append(int(input(f'Digite o valor para [0,{a}]: ')))
-
-for b in range(0,3):
-    linha2.append(int(input(f'Digite o valor para [1,{b}]: ')))
-    if b == 0:
-        maior = menor = linha2[b]
-    else:
-        if linha2[b] > maior:
-            maior = linha2[b]
-        if linha2[b] < menor:
-            menor = linha2[b]
-
-for c in range(0,3):
-    linha3.append(int(input(f'Digite o valor para [2,{c}]: ')))
-
-lista.append(linha1[:])
-lista.append(linha2[:])
-lista.append(linha3[:])
-
-for d in linha1:
-    if d % 2 == 0:
-        pares.append(d)
-    if d % 2 == 1:
-       impares.append(d)
-for e in linha2:
-    if e % 2 == 0:
-        pares.append(e)
-    if e % 2 == 1:
-       impares.append(e)
-for f in linha2:
-    if f % 2 == 0:
-        pares.append(f)
-    if f % 2 == 1:
-       impares.append(f)
-
-print(f'[  {lista[0][0]}  ][  {lista[0][1]}  ][  {lista[0][2]}  ]')
-print(f'[  {lista[1][0]}  ][  {lista[1][1]}  ][  {lista[1][2]}  ]')
-print(f'[  {lista[2][0]}  ][  {lista[2][1]}  ][  {lista[2][2]}  ]')
+matriz = [[0,0,0], [0,0,0], [0,0,0]]
+spar = maior = scol = 0
+for l in range(0,3):
+    for c in range(0,3):
+        matriz[l][c] = int(input(f'Digite um valor para {l},{c}: '))
 print('='*30)
-print(f'A soma de todas os valores pares digitados é {sum(pares)}')
-print(f'A soma dos valores da terceira coluna é {lista[0][2]+lista[1][2]+lista[2][2]}.')
+for l in range(0,3):
+    for c in range(0,3):
+        print(f'[{matriz[l][c]:^5}]', end='')
+        if matriz[l][c] % 2 == 0:
+            spar += matriz[l][c]
+    print()
+print('='*30)
+print(f'A soma dos valores pares é {spar}.')
+for l in range(0,3):
+    scol += matriz[l][2]
+print(f'A soma dos valores da terceira coluna é {scol}.')
+for c in range(0,3):
+    if c == 0:
+        maior = matriz[1][c]
+    elif matriz[1][c] > maior:
+        maior = matriz[1][c]
 print(f'O maior valor da segunda linha é {maior}.')
-
-
-
